696505e45ff1fc16c87e9a5886f497d2e2d44bab
swieczak <tosiakulka@gmail.com>
2020-05-22 19:17:39 +0200

    friendsy

---
+++ friend.cpp
@@ -0,0 +1,30 @@
+#include"friend.h"
+
+Point::Point(float xx, float yy)
+{
+	x = xx;
+	y = yy;
+
+}
+void Point::read()
+{
+	cout << "Podaj wsporzedne punktu: " << endl;
+	cout << "x: "; cin >> x;
+	cout << "y: "; cin >> y;
+	cout << endl;
+
+}
+Circle::Circle(float xx, float yy, float rr)
+{
+	x = xx;
+	y = yy;
+	r = rr;
+}
+void Circle::read()
+{
+	cout << "Podaj wspolrzedne srodka oraz promien okregu: " << endl;
+	cout << "x: "; cin >> x;
+	cout << "y: "; cin >> y;
+	cout << "r: "; cin >> r;
+
+}
